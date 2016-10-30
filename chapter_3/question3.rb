876195cd5a0162b86e9c31743acc14fe7ea9b45a
Andrew Eskenazi <Drewesk@gmail.com>
2016-10-30 16:32:32 -0600

    Followed ch_3 lesson and completed the HW
    * Created ex3.rb and followed instructions.
    
    * Created ex3_floating.rb to convert code in ex3.rb to floating point from integer math.
    
    * Filled out table in the Readme.md.
    
    * Answered question 3 in question3.rb.
    
    * Rubocop offense free.
    
    [#131482409]

---
+++ chapter_3/question3.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+# 3) Find something you need to calculate and write a new .rb file that does it.
+# I'm calculating how many 16 fluid oz portions in a ten gallon batch of tea.
+puts 'How guests can you serve a 16oz portion,'
+puts 'when making a ten gallon batch of tea?'
+puts "#{10 * 128 / 16} guests."
